a3b5e06723482f2ab39c1b3c3fb8d3b2096f8321
Rob_Kesterson <Simon.Illyan@gmail.com>
2021-12-13 22:41:30 -0800

    Update main.cpp

--- P3_url_parser/main.cpp
+++ P3_url_parser/main.cpp
@@ -7,11 +7,9 @@ int main() {
 	std::string input_url;
 	std::cin >> input_url;
 	int colon_marker = input_url.find(":");
-	//int double_slash_marker = input_url.find("//");
 	int single_slash_marker = input_url.find("/", colon_marker + 3);
 	
 	std::cout << "scheme = " + input_url.substr(0, colon_marker + 1) + "\n";
 	std::cout << "authority = " + input_url.substr(colon_marker + 1,single_slash_marker - colon_marker - 1) + "\n";
 	std::cout << "path = " + input_url.substr(single_slash_marker) + "\n";
-	std::cout << "hello";
 }
